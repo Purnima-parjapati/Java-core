98347b7531cd42e50c600b9c04c0c355bc16e58d
Purnima-parjapati <payalparjapati22@gmail.com>
2025-03-30 23:17:56 +0530

    Create Student.java
    Assignment-1.
    
    • Create a class Student in Student.java then add member variables studentName, collegeName of type String
    
    • Add a member variable studentID of type int.
    
    • Make all the member variables as private.
    
    • Add a main method. And print a message "Successful".
    
    • Compile the class
    
    • Run the class

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    private String studentName ;
+    private String collegeName ;
+    private int StudentId ;
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.studentName = "Purnima";
+        System.out.println(student.studentName);
+        System.out.println("sucessfull");
+    }
+}
