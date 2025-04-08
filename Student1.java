8703c20da9f1274b47fc9e58fd81dfd414a08e4c
Purnima-parjapati <payalparjapati22@gmail.com>
2025-04-08 21:53:21 +0530

    Create Student1.java
    Encapsulation using getter setter...

---
+++ Student1.java
@@ -0,0 +1,26 @@
+// Class - a class is a blueprint or template for creating objects.
+
+// Methods - A method in Java is a block of code that performs a specific task.
+package DT;
+
+public class Student1 {
+     private int age = 20;
+     private String  name = "Arun";
+
+
+     void  setAge(int age){
+         this.age = age;
+     }
+
+     public void setName(String name){
+         this.name = name;
+     }
+
+     int getAge(){
+         return age;
+     }
+      String getName(){
+         return name;
+      }
+
+ }
